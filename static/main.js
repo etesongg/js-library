9239e9dda13e77a1b4b7aa5f83fb6c802b019f18
kyuri <yimkyuri@naver.com>
2024-02-28 01:15:53 +0900

    feat: 인기대출 도서 끌어오기

--- static/main.js
+++ static/main.js
@@ -29,3 +29,39 @@ const enterkey = () => {
         getBooksByKeyword();
     }
 }
+
+
+
+<!-- 인기대출도서 조회 -->
+let popularBooks_Url = new URL(`http://data4library.kr/api/loanItemSrch?format=json&authKey=${API_KEY}&pageNo=1&pageSize=5`);
+
+<!-- 인기대출도서 불러오는 함수 -->
+const popularBooks = async () => {
+    try {
+        const response2 = await fetch(popularBooks_Url);
+        const data2 = await response2.json();
+        const popularBooksList = data2.response.docs;
+        console.log(popularBooksList);
+
+        const resultHTML = popularBooksList.map(book => { return `
+            <article>
+                <a href="${book.doc.bookDtlUrl}" target="_blank">
+                    <p class="book-rank">${book.doc.ranking}</p>
+                    <figure>
+                        <img src="${book.doc.bookImageURL}" alt="${book.doc.bookname}">
+                    </figure>
+                    <p class="book-title">${book.doc.bookname}</p>
+                    <p class="book-authers">${book.doc.authors}</p>
+                </a>
+            </article>
+        `}).join('');
+
+        document.querySelector('#popular-books-section .slider-wrap').innerHTML = resultHTML;
+        
+    } catch (error) {
+        console.error('Fetching book data failed', error);
+    }
+};
+
+popularBooks();
+
