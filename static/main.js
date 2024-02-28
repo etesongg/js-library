283c25f8ed93ff1b6959d76b1d655e26dea2e88b
Seungwon Lee <dvlplee16@gmail.com>
2024-02-28 20:13:36 +0900

    docs : list.html 수정

--- static/main.js
+++ static/main.js
@@ -3,28 +3,31 @@ const API_KEY = config.librarykey;
 let bookList = [];
 let keyword = "";
 
-let url = new URL(`http://data4library.kr/api/libSrch?format=json&authKey=${API_KEY}&pageNo=1&pageSize=5`);
+let url = new URL(
+  `http://data4library.kr/api/libSrch?format=json&authKey=${API_KEY}&pageNo=1&pageSize=5`
+);
 
-const getBooks = async() => {
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data)
-    const numFound = await data.response.numFound.toLocaleString()
-    
-    // 검색결과 건수 나오는 부분으로 render 함수가 생기면 render 함수 위치로 변경 예정
-    document.querySelector(".search_result").innerHTML = `"${keyword}" 검색결과 ${numFound}건`;
-}
+const getBooks = async () => {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+  const numFound = await data.response.numFound.toLocaleString();
 
-document.addEventListener("DOMContentLoaded", function() {
-    const queryParams = new URLSearchParams(window.location.search);
-    const page = queryParams.get('page');
-    
-    if (page !== 'details' && page !== 'list') {
-        // 'details or list' 페이지가 아닐 때만 getBooks 함수를 실행합니다.
-        getBooks();
-    }
+  // 검색결과 건수 나오는 부분으로 render 함수가 생기면 render 함수 위치로 변경 예정
+  document.querySelector(
+    ".search_result"
+  ).innerHTML = `"${keyword}" 검색결과 ${numFound}건`;
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  const queryParams = new URLSearchParams(window.location.search);
+  const page = queryParams.get("page");
+
+  if (page !== "details" && page !== "list") {
+    // 'details or list' 페이지가 아닐 때만 getBooks 함수를 실행합니다.
+    getBooks();
+  }
 });
-
 
 const getBooksByKeyword = async () => {
   keyword = document.getElementById("search-input").value;
@@ -35,8 +38,9 @@ const getBooksByKeyword = async () => {
 
   const response = await fetch(url);
   const data = await response.json();
+  console.log("ddd", data)
   bookList = data.response.docs;
-  console.log("ddd", bookList);
+  console.log("LLL", bookList);
   // searchRender 부분
   searchRender();
 };
@@ -46,7 +50,11 @@ const searchRender = () => {
     .map(
       (books) => `<div class="row books">
     <div class="col-lg-4">
-        <img class="books-img-size" src=${books.doc.bookImageURL}>
+        <img class="books-img-size"
+                src="${
+                  books.doc.bookImageURL ||
+                  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqEWgS0uxxEYJ0PsOb2OgwyWvC0Gjp8NUdPw&usqp=CAU"
+                }" />
     </div>
     <div class="col-lg-8">
         <h5>${books.doc.bookname}</h5>
